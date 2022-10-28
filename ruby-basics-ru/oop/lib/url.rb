dd4e9fcf5fea5a7ba36c85920dc82d927b07ac7a
asagafonov <a.s.agafonov@yandex.ru>
2022-10-28 13:29:17 +0300

    add oop assignment

---
+++ ruby-basics-ru/oop/lib/url.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'uri'
+require 'forwardable'
+
+# BEGIN
+class Url
+  attr_reader :url, :uri_url
+
+  include Comparable
+  extend Forwardable
+  def_delegator :uri_url, :scheme, :scheme
+  def_delegator :uri_url, :host, :host
+
+  def initialize(url)
+    @url = url
+    @uri_url = URI(url)
+  end
+
+  def query_params
+    query = uri_url.query
+    return {} unless query
+
+    params = query.split('&')
+    pairs = params.map { |p| p.split('=') }
+    pairs.to_h.transform_keys(&:to_sym)
+  end
+
+  def query_param(key, val = nil)
+    params = query_params
+    params.key?(key) ? params[key] : val
+  end
+
+  def <=>(other)
+    url <=> other.url
+  end
+end
+# END
