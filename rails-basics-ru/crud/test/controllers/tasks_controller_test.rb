1fe1ee69c8e50b65b06d19ec3be19404021040b2
asagafonov <a.s.agafonov@yandex.ru>
2022-11-29 15:59:05 +0300

    revert crud

--- rails-basics-ru/crud/test/controllers/tasks_controller_test.rb
+++ rails-basics-ru/crud/test/controllers/tasks_controller_test.rb
@@ -3,56 +3,8 @@ require "test_helper"
 class TasksControllerTest < ActionDispatch::IntegrationTest
   self.use_transactional_tests = true
 
-  setup do
-    @task = tasks(:one)
-    @attrs = {
-      name: Faker::Artist.name,
-      description: Faker::Movies::HarryPotter.quote,
-      status: Faker::Movies::HarryPotter.spell,
-      creator: Faker::Movies::HarryPotter.character,
-      performer: Faker::Movies::HarryPotter.character,
-      completed: Faker::Boolean.boolean
-    }
-  end
-
-  test 'should get index' do
+  test 'index page is displayed correctly' do
     get tasks_url
     assert_response :success
-  end
-
-  test 'should get new' do
-    get new_task_url
-    assert_response :success
-  end
-
-  test 'should create task' do
-    post tasks_url params: { task: @attrs }
-    task = Task.find_by! name: @attrs[:name]
-    assert_redirected_to task_url(task)
-  end
-
-  test 'should show task' do
-    get task_url(@task)
-    assert_response :success
-  end
-
-  test 'should edit task' do
-    get edit_task_url(@task)
-    assert_response :success
-  end
-
-  test 'should update task' do
-    patch task_url(@task), params: { task: @attrs }
-    assert_redirected_to task_url(@task)
-
-    @task.reload
-    assert { @task.name == @attrs[:name] }
-  end
-
-  test 'should destroy' do
-    delete task_url(@task)
-
-    assert_redirected_to tasks_url
-    assert { !Task.exists? @task.id }
   end
 end
